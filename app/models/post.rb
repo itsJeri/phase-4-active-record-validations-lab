50af3d6f71b28e2ee8477b3bf5709ec18e0084a4
Jerry Tong <zihangtong@gmail.com>
2022-02-01 21:45:56 -0500

    commit tree be8d8f928add8546c82b10e780615d8ce9a0b9de

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :must_be_clickbait
+
+  def must_be_clickbait
+    if [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i].none?{
+      |pattern| pattern.match(title)
+    }
+      errors.add(:title, "Not clickbait-y enough!")
+    end
+  end
 end
